26c2db518b101c35d7a549d0174ad96f52de4697
SamChen <sam159247@gmail.com>
2021-12-05 23:32:23 +0900

    fix: avoid get ssm even in local environment
    Due to mechanism of pydantic will automatically read environment.

--- app/core/config.py
+++ app/core/config.py
@@ -7,50 +7,50 @@ from pydantic import BaseSettings
 
 from app.core.utils import get_ssm_parameters
 
+# TODO: refactor
+ENVIRONMENT: str = os.getenv("ENV", "DEVELPOMENT")
+
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "linetwstock"
-    ENV: str
+    ENV: str = ENVIRONMENT
     FINMIND_BASE_URL: str = "https://api.finmindtrade.com/api/v4/data"
     FINMIND_TOKEN: str
 
 
 class Development(Settings):
-    ENV = "DEVELPOMENT"
-
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
 
 
 class Staging_1(Settings):
-    ENV = "STAGING_1"
-    result_params = get_ssm_parameters(
-        [
-            "/stg1/lambda/linetwstock/finmind_token",
-        ]
-    )
+    if ENVIRONMENT == "STAGING_1":
+        result_params = get_ssm_parameters(
+            [
+                "/stg1/lambda/linetwstock/finmind_token",
+            ]
+        )
 
-    FINMIND_TOKEN = result_params["/stg1/lambda/linetwstock/finmind_token"]
+        FINMIND_TOKEN = result_params["/stg1/lambda/linetwstock/finmind_token"]
 
 
 class Production(Settings):
-    ENV = "PRODUCTION"
-    result_params = get_ssm_parameters(
-        [
-            "/prod/lambda/linetwstock/finmind_token",
-        ]
-    )
+    if ENVIRONMENT == "PRODUCTION":
+        result_params = get_ssm_parameters(
+            [
+                "/prod/lambda/linetwstock/finmind_token",
+            ]
+        )
 
-    FINMIND_TOKEN = result_params["/prod/lambda/linetwstock/finmind_token"]
+        FINMIND_TOKEN = result_params["/prod/lambda/linetwstock/finmind_token"]
 
 
 @cache
 def get_settings() -> Settings:
-    env = os.getenv("ENV", "DEVELPOMENT")
-    if env == "STAGING_1":
+    if ENVIRONMENT == "STAGING_1":
         return Staging_1()
-    if env == "PRODUCTION":
+    if ENVIRONMENT == "PRODUCTION":
         return Production()
     return Development()
 
